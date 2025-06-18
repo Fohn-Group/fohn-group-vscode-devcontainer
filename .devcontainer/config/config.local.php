67526b0141fec9f8cb6d0bff053fad22a6e26512
Alain Belair <belair.alain@gmail.com>
2025-06-18 16:31:21 -0400

    update gitignore

---
+++ .devcontainer/config/config.local.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Fohn\Ui\Service\Ui;
+
+// Overide values for you local environment.
+return [
+    //    'db' => new Atk4\Data\Persistence\Sql('mysql:dbname=fohn;host=mysql;charset=utf8', 'root', 'root'),
+    // 'env' => Ui::DEV_ENV,
+    'timezone' => 'America/New_York',
+];
