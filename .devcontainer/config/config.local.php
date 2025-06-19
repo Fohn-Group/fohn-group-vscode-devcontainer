e8aa9bac9f59828c8f2539c1a5f8bf126dd5b84b
Alain Belair <belair.alain@gmail.com>
2025-06-19 13:55:47 -0400

    Enhance local development configuration and add PHP settings
    - Updated config.local.php with detailed comments for database and environment settings.
    - Added fohn-conf.ini for PHP configuration settings.
    - Modified start.sh to include copying of PHP config.
    - Created tasks.json for CS-Fixer and PHPStan tasks.

--- .devcontainer/config/config.local.php
+++ .devcontainer/config/config.local.php
@@ -6,7 +6,17 @@ use Fohn\Ui\Service\Ui;
 
 // Overide values for you local environment.
 return [
-    //    'db' => new Atk4\Data\Persistence\Sql('mysql:dbname=fohn;host=mysql;charset=utf8', 'root', 'root'),
-    // 'env' => Ui::DEV_ENV,
+    /**
+     * Database connection.
+     * Uncomment this line in order to use the MySQL database.
+     * Make sure to set the correct credentials for your MySQL server.
+     */
+    // 'db' => new Atk4\Data\Persistence\Sql('mysql:dbname=fohn;host=mysql;charset=utf8', 'root', 'root'),
+    /**
+     * Environment setting.
+     * Uncomment this line in order to develop fohj-js and foh-css.
+     * You will need to run `npm run dev` in the fohn-js and fohn-css directories.
+     */
+    // 'env' => Ui::DEV_ENV, 
     'timezone' => 'America/New_York',
 ];
--- .devcontainer/script/start.sh
+++ .devcontainer/script/start.sh
@@ -8,11 +8,13 @@ npm install --prefix fohn-js
 echo "install css dependencies"
 npm install --prefix fohn-css
 
+echo "Copying php config"
+sudo cp .devcontainer/config/php/fohn-conf.ini /usr/local/etc/php/conf.d/fohn-conf.ini
 
-echo "Copying local config"
+
+echo "Copying app local config"
 target_dir="fohn-ui/local"
 mkdir -p "$target_dir" && cp .devcontainer/config/config.local.php "$target_dir/"
-
 
 
 echo "Starting Apache server..."
