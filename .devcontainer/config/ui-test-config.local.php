9c8b441334b05452eb467c8ca996659dcd640b42
Alain Belair <belair.alain@gmail.com>
2025-07-30 07:31:35 -0400

    add test-config local

---
+++ .devcontainer/config/ui-test-config.local.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'env' => 'test-local',
+    'base_uri' => 'http://127.0.0.1:8080',
+    'x-coverage-value' => '', // do not generate coverage on http request.
+];
